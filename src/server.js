caa2456d87d5a4471c807fb2f418782cad9981d3
haemiii <73984822+haemiii@users.noreply.github.com>
2022-04-15 00:22:01 +0900

    passport를 이용한 login 구현

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
 import passport from "passport";
+
+import backRouter from "./routers/backRouter.js";
+
+import "./passport.js";
 
 const app = express();
 
@@ -12,6 +19,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -20,5 +36,20 @@ app.use((req, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
+
+app.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/fail" }),
+  function (req, res) {
+    res.redirect("/success");
+  }
+);
+
+app.use("/", backRouter);
 
 app.listen(4000, () => console.log(`Example app listening on port ${4000}!`));
